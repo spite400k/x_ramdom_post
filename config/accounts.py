e499724ce44df4b31bb25156d2bdbadd00236d11
spite400k <spite400k@gmail.com>
2025-07-15 07:10:23 +0900

    修正

--- config/accounts.py
+++ config/accounts.py
@@ -4,22 +4,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ACCOUNTS = [
+    # {
+    #     "API_KEY": os.getenv("API_KEY_1"),
+    #     "API_SECRET": os.getenv("API_SECRET_KEY_1"),
+    #     "ACCESS_TOKEN": os.getenv("ACCESS_TOKEN_1"),
+    #     "ACCESS_TOKEN_SECRET": os.getenv("ACCESS_TOKEN_SECRET_1")
+    # },
+    # {
+    #     "API_KEY": os.getenv("API_KEY_2"),
+    #     "API_SECRET": os.getenv("API_SECRET_KEY_2"),
+    #     "ACCESS_TOKEN": os.getenv("ACCESS_TOKEN_2"),
+    #     "ACCESS_TOKEN_SECRET": os.getenv("ACCESS_TOKEN_SECRET_2")
+    # },
+    # {
+    #     "API_KEY": os.getenv("API_KEY_3"),
+    #     "API_SECRET": os.getenv("API_SECRET_KEY_3"),
+    #     "ACCESS_TOKEN": os.getenv("ACCESS_TOKEN_3"),
+    #     "ACCESS_TOKEN_SECRET": os.getenv("ACCESS_TOKEN_SECRET_3")
+    # },
     {
-        "API_KEY": os.getenv("API_KEY_1"),
-        "API_SECRET": os.getenv("API_SECRET_KEY_1"),
-        "ACCESS_TOKEN": os.getenv("ACCESS_TOKEN_1"),
-        "ACCESS_TOKEN_SECRET": os.getenv("ACCESS_TOKEN_SECRET_1")
-    },
-    {
-        "API_KEY": os.getenv("API_KEY_2"),
-        "API_SECRET": os.getenv("API_SECRET_KEY_2"),
-        "ACCESS_TOKEN": os.getenv("ACCESS_TOKEN_2"),
-        "ACCESS_TOKEN_SECRET": os.getenv("ACCESS_TOKEN_SECRET_2")
-    },
-    {
-        "API_KEY": os.getenv("API_KEY_3"),
-        "API_SECRET": os.getenv("API_SECRET_KEY_3"),
-        "ACCESS_TOKEN": os.getenv("ACCESS_TOKEN_3"),
-        "ACCESS_TOKEN_SECRET": os.getenv("ACCESS_TOKEN_SECRET_3")
+        "API_KEY": os.getenv("API_KEY_4"),
+        "API_SECRET": os.getenv("API_SECRET_KEY_4"),
+        "ACCESS_TOKEN": os.getenv("ACCESS_TOKEN_4"),
+        "ACCESS_TOKEN_SECRET": os.getenv("ACCESS_TOKEN_SECRET_4")
     },
 ]
